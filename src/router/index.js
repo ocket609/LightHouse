dfedd82413d8eec200c6e5cf1dbf21158ffa54e4
yuling <gb2013429@gmail.com>
2024-09-13 22:24:28 +0800

    fix index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,33 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { createRouter, createWebHashHistory } from 'vue-router'
+import ConsumerLayout from '../views/consumer/ConsumerLayout.vue'
+import ConsumerHome from '../views/consumer/HomePage.vue'
+
 
 const router = createRouter({
   history: createWebHashHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: HomeView
+      path: '/consumer',
+      name: 'consumer',
+      component: ConsumerLayout,
+      children: [
+        {
+          path: 'home',
+          name: 'consumerHome',
+          component: ConsumerHome,
+          meta: { title: '燈塔網站' },
+        },
+      ]
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
+      path: '/:pathMatch(.*)*',
+      name: 'notFount',
+      redirect: '/consumer/home'
     }
   ]
 })
-
+router.beforeEach((to, from, next) => {
+  document.title = to.meta.title || '默認標題'
+  next()
+})
 export default router
