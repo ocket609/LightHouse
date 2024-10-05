fbf0aa9899036f25b5d6158f1295b66a4a334196
yuling <gb2013429@gmail.com>
2024-10-05 19:57:44 +0800

    feat change text

--- src/components/consumer/homePage/LocationComponent.vue
+++ src/components/consumer/homePage/LocationComponent.vue
@@ -16,8 +16,8 @@ const tabs = ref([
             <div class="fs-1 d-flex justify-content-center">LIGHTHOUSE</div>
             <div class="d-flex justify-content-center" style="margin-top: 160px;">
                 <div class="location-title mb-16">
-                    <h3 class="mb-6" style="letter-spacing:16px;">週邊小物</h3>
-                    <p style="letter-spacing:4px;">LIGHTHOUSE STORE</p>
+                    <h3 class="mb-6" style="letter-spacing:16px;">燈塔分佈</h3>
+                    <p style="letter-spacing:4px;">LIGHTHOUSE LOCATION</p>
                     <div class="d-flex justify-content-center">
                         <div class="border border-2 border-primary" style="width: 120px;"></div>
                     </div>
