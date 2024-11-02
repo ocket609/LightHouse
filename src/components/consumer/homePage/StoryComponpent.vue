1f436a23f657a4703a4dcdd7f72dd08ed4d63b75
QOO <ocket609@gmail.com>
2024-11-02 16:38:36 +0800

    light hover fix

--- src/components/consumer/homePage/StoryComponpent.vue
+++ src/components/consumer/homePage/StoryComponpent.vue
@@ -2,8 +2,8 @@
   <div class="homeStory">
     <div class="homeStory-main story-container text-white d-flex flex-column align-items-center">
       <div class="homeStory-title fw-bold d-flex flex-column align-items-center py-1 mb-16">
-        <p class="fs-7 fs-sm-3 mb-6">心靈雞湯</p>
-        <p class="mb-6">LIGHT HOUSE STORY</p>
+        <p class="fs-7 fs-sm-3 mb-6"><span class="letter-spacing-16">心靈雞</span>湯</p>
+        <p class="fs-9 fs-sm-6 mb-6"><span class="letter-spacing-4">LIGHT HOUSE STOR</span>Y</p>
         <div class="border-line"></div>
       </div>
       <div class="homeStory-content row mx-0">
@@ -13,7 +13,6 @@
             是燈塔人的驕傲
           </p>
           <div class="homeStory-content-card-hover">
-            <div class="homeStory-light-hover"></div>
             <div class="homeStory-content-card-hover-text">
               <p class="text-center homeStory-content-card-text mb-0">
                 忍受寂寞扛起整個夜晚， 深夜裡的曙光<br />
@@ -22,6 +21,7 @@
               <p class="text-center text-primary fw-bold pt-6">MORE</p>
             </div>
           </div>
+          <div class="homeStory-light d-none d-md-block"></div>
         </div>
         <div class="col-12 col-xl-4 homeStory-content-card px-0">
           <p class="text-center homeStory-content-card-text">
