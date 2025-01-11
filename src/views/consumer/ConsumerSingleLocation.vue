55a1dfeb668be15e1bb424bda5d69a1c8df9d97e
yuling <gb2013429@gmail.com>
2025-01-11 18:57:43 +0800

    layout: add adblock + style-dark

--- src/views/consumer/ConsumerSingleLocation.vue
+++ src/views/consumer/ConsumerSingleLocation.vue
@@ -1,13 +1,18 @@
-<template>
-  <div>
-    <h1>燈塔詳情</h1>
-    <p>燈塔 ID: {{ route.params.singleLocationId }}</p>
-  </div>
-</template>
 <script setup>
-import { useRoute } from 'vue-router';
-
-const route = useRoute();
-console.log(route);
+import { useRoute } from 'vue-router'
+import AdComponent from '@/components/consumer/location/AdComponent.vue'
+const route = useRoute()
+console.log(route)
 window.scrollTo(0, 0)
 </script>
+<template>
+  <AdComponent />
+  <div class="bg-dark pt-5 pb-10">
+    <div class="container py-10 text-white">
+      <div>
+        <h1>燈塔詳情</h1>
+        <p>燈塔 ID: {{ route.params.singleLocationId }}</p>
+      </div>
+    </div>
+  </div>
+</template>
