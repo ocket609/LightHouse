b832cf3eb561d7ff307f91719ee438110fb80951
yuling <gb2013429@gmail.com>
2025-01-11 18:26:08 +0800

    fix: return-page-initMasonry in ConsumerLocation write onActived

--- src/views/consumer/ConsumerLocation.vue
+++ src/views/consumer/ConsumerLocation.vue
@@ -1,18 +1,27 @@
 <script setup>
-import { onMounted } from 'vue'
+import { onMounted, onActivated } from 'vue'
 import AdComponent from '@/components/consumer/location/AdComponent.vue'
 import SearchComponent from '@/components/consumer/location/SearchComponent.vue'
 import LHListComponent from '@/components/consumer/location/LHListComponent.vue'
 import LHListComponent2 from '@/components/consumer/location/LHListComponent2.vue'
 import { useLighthouseStore } from '@/stores/lightHouseList.js'
-
+import { useWaterfallStore } from '@/stores/waterfall.js'
+const waterfallStore = useWaterfallStore()
 const lighthouseStore = useLighthouseStore()
 // 在組件掛載時觸發資料加載
 onMounted(async () => {
   lighthouseStore.isLoading = true // 設置為加載中
+  if (lighthouseStore.lighthouses.length > 0) {
+    console.log(lighthouseStore.lighthouses.length);
+        waterfallStore.initMasonry() // 初始化 Masonry
+      }
   await lighthouseStore.getArticleData() // 獲取資料
   lighthouseStore.isLoading = false // 加載完成
 })
+    // 当页面重新激活时（如通过浏览器后退）触发布局
+    onActivated(() => {
+      waterfallStore.initMasonry() // 初始化 Masonry
+    });
 </script>
 <template>
   <AdComponent />
