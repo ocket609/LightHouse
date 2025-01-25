54b3d03e5b9c4cf3347b2faf7e4cdf0bb578446b
QOO <ocket609@gmail.com>
2025-01-25 16:21:34 +0800

    fix: Modify the order of homepages

--- src/views/consumer/HomePage.vue
+++ src/views/consumer/HomePage.vue
@@ -1,12 +1,12 @@
 <script setup>
 import BannerComponpent from '@/components/consumer/homePage/BannerComponpent.vue'
 import LocationComponent from '@/components/consumer/homePage/LocationComponent.vue'
-import StoreComponent from '@/components/consumer/homePage/StoreComponent.vue'
 import StoryComponpent from '@/components/consumer/homePage/StoryComponpent.vue'
+import StoreComponent from '@/components/consumer/homePage/StoreComponent.vue'
 </script>
 <template>
   <BannerComponpent />
   <LocationComponent />
-  <StoreComponent />
   <StoryComponpent />
+  <StoreComponent />
 </template>
